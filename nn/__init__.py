122777934c07b1a06c6ea987cda093b090bee4b1
Jakob Havtorn <jakob.havtorn@gmail.com>
2018-05-15 17:59:49 +0200

    Structure of library

--- nn/__init__.py
+++ nn/__init__.py
@@ -2,5 +2,7 @@ from .activation import *
 from .batchnorm import *
 from .linear import *
 from .loss import *
+# from .solver import Solver
+from .containers import Sequential
 from .module import Module
 from .parameter import Parameter
