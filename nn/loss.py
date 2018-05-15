6b153fa554e8baba84bc7efb6e201ade465a62b9
Jakob Havtorn <jakob.havtorn@gmail.com>
2018-05-15 18:00:44 +0200

    Subclasses of Module and renamed methods

--- nn/loss.py
+++ nn/loss.py
@@ -1,36 +1,31 @@
 import numpy as np
+from .module import Module
 
 
-class MeanSquaredLoss():
+class MeanSquaredLoss(Module):
     def __str__(self): 
         return "MeanSquaredLoss()"
     
-    def fprop(self, x, t):
+    def forward(self, x, t):
         num_batches = x.shape[0]
         cost = 0.5 * (x-t)**2 / num_batches
         return np.mean(np.sum(cost, axis=-1))
         
-    def bprop(self, y, t):
+    def backward(self, y, t):
         num_batches = y.shape[0]
         delta_out = (1.0/num_batches) * (y-t)
         return delta_out
         
-    def update_params(self):
-        pass
 
-
-class CrossEntropyLoss():
+class CrossEntropyLoss(Module):
     def __str__(self): 
         return "CrossEntropyLoss()"
     
-    def fprop(self, x, t):
+    def forward(self, x, t):
         tol = 1e-8
         return np.mean(np.sum(-t * np.log(x + tol), axis=-1))
         
-    def bprop(self, y, t):
+    def backward(self, y, t):
         num_batches = y.shape[0]
         delta_out = (1.0/num_batches) * (y-t)
         return delta_out
-        
-    def update_params(self):
-        pass
