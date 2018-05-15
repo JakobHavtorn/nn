07c72e712134949ccbd3c6ebf65f22bcccc8245d
Jakob Havtorn <jakob.havtorn@gmail.com>
2018-05-15 17:59:26 +0200

    Better representation

--- nn/parameter.py
+++ nn/parameter.py
@@ -9,10 +9,14 @@ class Parameter(object):
     def __str__(self):
         s = "Parameter containing:\n"
         s += self.data.__repr__()
+        s += "\n" + '(' + ', '.join([str(s) for s in self.data.shape]) + ')'
         if self.grad is not None:
             s += "\nand gradient\n"
             s += self.grad.__repr__()
+            s += "\n" + '(' + ', '.join([str(s) for s in self.data.shape]) + ')'
+        s += "\n"
         return s
 
+    @property
     def shape(self):
         return self.data.shape
