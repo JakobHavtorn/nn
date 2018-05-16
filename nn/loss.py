c8d86a5c8eb08112091a5bb638996afb880c6709
Jakob Havtorn <jakob.havtorn@gmail.com>
2018-05-16 13:30:17 +0200

    Added documentation and fixed cross entropy loss

--- nn/loss.py
+++ nn/loss.py
@@ -1,14 +1,15 @@
 import numpy as np
 from .module import Module
+import IPython
 
 
 class MeanSquaredLoss(Module):
     def __str__(self): 
         return "MeanSquaredLoss()"
     
-    def forward(self, x, t):
-        num_batches = x.shape[0]
-        cost = 0.5 * (x-t)**2 / num_batches
+    def forward(self, y, t):
+        num_batches = y.shape[0]
+        cost = 0.5 * (y-t)**2 / num_batches
         return np.mean(np.sum(cost, axis=-1))
         
     def backward(self, y, t):
@@ -18,14 +19,25 @@ class MeanSquaredLoss(Module):
         
 
 class CrossEntropyLoss(Module):
+    """Cross Entropy Loss function for D class classification
+
+        Shape
+        -----
+        Input: 
+            (N, C) where N is batch size and C is number of classes.
+        Target: 
+            (N, C) where each row is a one-hot encoded vector
+        Output: float
+            Scalar loss.
+    """
+
     def __str__(self): 
         return "CrossEntropyLoss()"
     
-    def forward(self, x, t):
-        tol = 1e-8
-        return np.mean(np.sum(-t * np.log(x + tol), axis=-1))
+    def forward(self, y, t, eps=1e-8):
+        return - np.mean(np.log(y[t] + eps), axis=-1)
+        # return np.mean(np.sum(-t * np.log(y + eps), axis=-1))
         
     def backward(self, y, t):
-        num_batches = y.shape[0]
-        delta_out = (1.0/num_batches) * (y-t)
+        delta_out = (1.0 / y.shape[0]) * (y - t)
         return delta_out
