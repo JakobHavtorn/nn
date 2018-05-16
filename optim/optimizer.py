d28c90ef0f013bef47fde6fabb80bab3a5c9c730
Jakob Havtorn <jakob.havtorn@gmail.com>
2018-05-16 13:31:46 +0200

    Added common attributes for all optimizers (learning rate, weight decay)

--- optim/optimizer.py
+++ optim/optimizer.py
@@ -2,16 +2,16 @@ import numpy as np
 
 
 class Optimizer(object):
-    def __init__(self, module):
-        self.module = module
-        for p in self.parameters():
+    def __init__(self, model, **kwargs):
+        self.model = model
+        self.lr = kwargs.pop('lr')
+        self.weight_decay = kwargs.pop('weight_decay')
+        self.state = {}
+        for p in self.model.parameters():
             self.state[p] = {}
 
-    def parameters(self):
-
-
     def zero_grad(self):
-        for p in self.parameters():
+        for p in self.model.parameters():
             if p.grad is not None:
                 p.grad = np.zeros(p.shape)
 
