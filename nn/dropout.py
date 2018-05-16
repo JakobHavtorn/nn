8cac45a5aeae34902a1fda8a15186e4cab88a2e5
Jakob Havtorn <jakob.havtorn@gmail.com>
2018-05-16 14:41:35 +0200

    Added super initializer and changed p to be dropout rate (not keep)

--- nn/dropout.py
+++ nn/dropout.py
@@ -1,31 +1,35 @@
 import numpy as np
+
 from .module import Module
 
 
 class Dropout(Module):
-    def __str__(self): 
-        return "Dropout({:5.2f})".format(self.p)
-
+    """Dropout layer which randomly sets activations to zero.
+    
+    Parameters
+    ----------
+    p : float
+        The probability of zeroing any activation. Must be smaller than 1 and larger than 0
+    """
     def __init__(self, p=0.5):
+        super(Dropout, self).__init__()
         self.p = p
 
-    def fprop(self, x, train=True):
+    def __str__(self): 
+        return "Dropout({:.2f})".format(self.p)
+
+    def forward(self, x, train=True):
         if self.training:
-            mask = np.random.random(x.shape) > (1-self.p)
+            mask = np.random.random(x.shape) > self.p
             scale = 1.0 / (1-self.p)
             self.a = x*mask*scale
             return self.a
         else:
             return x
 
-    def bprop(self, delta_in):
+    def backward(self, delta_in):
         delta_out = delta_in*self.a
         return delta_out
 
     def update_params(self, lr):
         pass
-
-
-class GaussianDropout():
-    raise NotImplementedError()
-
