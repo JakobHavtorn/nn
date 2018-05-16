afd72574fe75ba34e07f646c106956981da93dd1
Jakob Havtorn <jakob.havtorn@gmail.com>
2018-05-16 13:31:11 +0200

    Added documentation and corrected bias inclusion in forward pass

--- nn/linear.py
+++ nn/linear.py
@@ -5,6 +5,25 @@ import IPython
 
 
 class Linear(Module):
+    """Linear module which performs an affine transformation.
+
+    The forward transformation is
+        y = x * W + b
+    where the following dimensions hold true
+        x:  (N, D)
+        W:  (D, M)
+        b:  (M
+        y:  (N, M)
+
+    Parameters
+    ----------
+    in_features : int
+        The number of input features
+    out_features : int
+        The number of output features
+    bias : bool, optional
+        Whether or not to include a bias (the default is True)
+    """
     def __init__(self, in_features, out_features, bias=True):
         super(Linear, self).__init__()
         self.in_features = in_features
@@ -17,7 +36,7 @@ class Linear(Module):
         self.reset_parameters()
 
     def __str__(self): 
-        return "Linear({d}, {d})".format(self.in_features, self.out_features)
+        return "Linear({:d}, {:d}, bias={})".format(self.in_features, self.out_features, self.b is not None)
 
     def reset_parameters(self):
         stdv = 1.0 / np.sqrt(self.W.shape[1])
@@ -27,15 +46,17 @@ class Linear(Module):
 
     def forward(self, x, *args):
         self.x = x
-        self.a = np.dot(x, self.W.data) + self.b.data
+        self.a = np.dot(x, self.W.data)
+        if self.b is not None:
+            self.a += self.b.data
         return self.a
         
     def backward(self, dout):
         self.W.grad = np.dot(self.x.T, dout)
+        dx = np.dot(dout, self.W.data.T)
         if self.b is not None:
             self.b.grad = dout.sum(axis=0)
-        din = np.dot(dout, self.W.data.T)
-        return din
+        return dx
 
     def update_params(self, lr):
         self.W.data = self.W.data - self.grad_W*lr
