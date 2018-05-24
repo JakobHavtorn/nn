6eaf18d2f6367cc471282e1cb9fab46f7b3aac34
Jakob Havtorn <jakob.havtorn@gmail.com>
2018-05-24 13:15:41 +0200

    Cleaned

--- nn/__init__.py
+++ nn/__init__.py
@@ -3,7 +3,6 @@ from .batchnorm import *
 from .dropout import *
 from .linear import *
 from .loss import *
-# from .solver import Solver
 from .containers import Sequential
 from .module import Module
 from .parameter import Parameter
