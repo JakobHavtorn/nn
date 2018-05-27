eb991d9d4be67d66a579ec5874420be8ee7fec1f
JakobHavtorn <jakob.havtorn@gmail.com>
2018-05-27 23:25:18 +0200

    Structure

--- nn/__init__.py
+++ nn/__init__.py
@@ -2,6 +2,9 @@ from .activation import *
 from .batchnorm import *
 from .dropout import *
 from .linear import *
+from .convolution import *
+from .pooling import *
+from .reshape import *
 from .loss import *
 from .containers import Sequential
 from .module import Module
