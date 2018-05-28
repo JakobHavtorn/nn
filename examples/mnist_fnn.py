a9304df5c546963be373bc008b9893529362015e
Jakob Havtorn <jakob.havtorn@gmail.com>
2018-05-28 12:54:12 +0200

    Updated examples

--- examples/mnist_fnn.py
+++ examples/mnist_fnn.py
@@ -1,3 +1,5 @@
+import os
+
 import IPython
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,26 +8,23 @@ from context import nn, optim, utils
 from loaders import get_loaders
 from models import FNNClassifier
 
+
 if __name__ == '__main__':
     # Model
     classifier = FNNClassifier(28 * 28, 10, hidden_dims=[512, 256, 128], activation=nn.ReLU, batchnorm=True, dropout=False)
-    for n, m in classifier.named_modules():
-        print(n, m)
-    n = 0
-    for p in classifier.parameters():
-        n += np.prod(p.shape)
-    print("Total number of parameters: " + str(n))
+    classifier.summarize()
     # Dataset
     save_dir = './results/mnist/'
     dataset_name = "MNIST"
-    batch_size = 64
+    batch_size = 250
+    num_epochs = 10
     train_loader, val_loader = get_loaders(dataset_name, batch_size)
     # Optimizer
-    optimizer = optim.SGD(classifier, lr=0.001, momentum=0.9, nesterov=False, dampening=0, l1_weight_decay=0, l2_weight_decay=0)
+    optimizer = optim.SGD(classifier, lr=0.01, momentum=0.9, nesterov=False, dampening=0, l1_weight_decay=0, l2_weight_decay=0)
     # Loss
     loss = nn.CrossEntropyLoss()
     # Train
-    solver = utils.Solver(classifier, train_loader, val_loader, optimizer, loss)
+    solver = utils.Solver(classifier, train_loader, val_loader, optimizer, loss, num_epochs=num_epochs, lr_decay=1.0)
     solver.train()
 
     if not os.path.exists(save_dir):
@@ -39,8 +38,8 @@ if __name__ == '__main__':
     a.set_xlabel('Iteration')
     a.set_ylabel('Negative log likelihod loss')
     a.legend(['Training', 'Validation'])
-    f.savefig(save_dir + 'loss.pdf', bbox_inches='tight')
-    f.savefig(save_dir + 'loss.png', bbox_inches='tight')
+    f.savefig(save_dir + 'loss_fnn.pdf', bbox_inches='tight')
+    f.savefig(save_dir + 'loss_fnn.png', bbox_inches='tight')
 
     f, a = plt.subplots()
     a.plot(solver.train_acc_history, '.', alpha=0.2,)
@@ -48,5 +47,5 @@ if __name__ == '__main__':
     a.set_xlabel('Iteration')
     a.set_ylabel('Classification accuracy')
     a.legend(['Training', 'Validation'])
-    f.savefig('./results/mnist/accuracy.pdf', bbox_inches='tight')
-    f.savefig('./results/mnist/accuracy.png', bbox_inches='tight')
+    f.savefig('./results/mnist/accuracy_fnn.pdf', bbox_inches='tight')
+    f.savefig('./results/mnist/accuracy_fnn.png', bbox_inches='tight')
