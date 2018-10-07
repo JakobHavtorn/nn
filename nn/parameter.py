3ffbba4b300e9d4496875219a73834744e8a1f60
Jakob Havtorn <jakob.havtorn@gmail.com>
2018-10-07 14:56:55 +0200

    Non-lazy gradient of Parameter

--- nn/parameter.py
+++ nn/parameter.py
@@ -4,14 +4,16 @@ import numpy as np
 class Parameter(object):
     def __init__(self, data):
         self.data = data
-        self.grad = None
+        self.grad = np.zeros_like(data)
+        # Gradient could be implemented lazily but that requires overloading
+        # the `+=` etc. operators
     
-    def __str__(self):
+    def __repr__(self):
         s = "Parameter containing:\n"
         s += self.data.__repr__()
         s += "\n" + '(' + ', '.join([str(s) for s in self.data.shape]) + ')'
         if self.grad is not None:
-            s += "\nand gradient\n"
+            s += "\n\nand gradient\n\n"
             s += self.grad.__repr__()
             s += "\n" + '(' + ', '.join([str(s) for s in self.data.shape]) + ')'
         s += "\n"
@@ -25,3 +27,57 @@ class Parameter(object):
         p = Parameter(self.data.copy())
         p.grad = self.grad.copy()
         return p
+
+
+class LazyArray(np.ndarray):
+    def __new__(cls, input_array):
+        # Input array is an already formed ndarray instance
+        # We first cast to be our class type
+        obj = input_array
+        # Finally, we must return the newly created object:
+        return obj
+
+    def __add__(self, data):
+        return self.data + data
+
+    def __sub__(self, other):
+        raise NotImplementedError()
+
+    def __mul__(self, other):
+        raise NotImplementedError()
+
+    def __div__(self, other):
+        raise NotImplementedError()
+
+    def __pow__(self, other):
+        raise NotImplementedError()
+
+    def __radd__(self, data):
+        raise NotImplementedError()
+
+    def __rsub__(self, data):
+        raise NotImplementedError()
+
+    def __rmul__(self, data):
+        raise NotImplementedError()
+
+    def __rdiv__(self, data):
+        raise NotImplementedError()
+
+    def __rpow__(self, data):
+        raise NotImplementedError()
+
+    def __iadd__(self, data):
+        raise NotImplementedError()
+
+    def __isub__(self, data):
+        raise NotImplementedError()
+
+    def __imul__(self, data):
+        raise NotImplementedError()
+
+    def __idiv__(self, data):
+        raise NotImplementedError()
+
+    def __ipow__(self, data):
+        raise NotImplementedError()
