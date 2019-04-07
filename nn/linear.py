e9bff2bfcd7de99c0494a51c7a463fd5096556c8
Jakob Havtorn <jakob.havtorn@gmail.com>
2019-04-07 17:58:41 +0200

    Start bilinear layer implementation

--- nn/linear.py
+++ nn/linear.py
@@ -5,23 +5,29 @@ from .module import Module
 from .parameter import Parameter
 
 
+# TODO Transpose linear W
+# TODO Make caches lists
+# TODO When backpropping loop over cached elements and accumulate gradients
+
+
 class Linear(Module):
     """Linear module which performs an affine transformation.
 
     The forward transformation is
         y = x * W + b
     with the following dimensions
-        x:  (N, D)
-        W:  (D, M)
-        b:  (M)
-        y:  (N, M)
+        x:  (N, *, I)
+        W:  (I, O)
+        b:  (O)
+        y:  (N, *, O)
+    where * means any number of additional dimensions.
 
     Parameters
     ----------
     in_features : int
-        The number of input features
+        The number of input features (I)
     out_features : int
-        The number of output features
+        The number of output features (O)
     bias : bool, optional
         Whether or not to include a bias (the default is True)
     """
@@ -53,8 +59,51 @@ class Linear(Module):
         return z
         
     def backward(self, delta):
-        self.W.grad = np.dot(self.x.T, delta)
+        self.W.grad += np.dot(self.x.T, delta)
         dx = np.dot(delta, self.W.data.T)
         if self.b is not None:
-            self.b.grad = delta.sum(axis=0)
+            self.b.grad += delta.sum(axis=0)
         return dx
+
+
+class BiLinear(Module):
+    """Bilinear module which performs a bilinear transformation.
+
+    The transformation is
+        y = x_1 * W * x_2 + b
+    with the following dimensions
+        x_1:  (N, *, I_1)
+        x_2:  (N, *, I_2)
+        W:    (O, I_1, I_2)
+        b:    (O)
+        y:    (N, O)
+
+    Parameters
+    ----------
+    in_features_1 : int
+        The number of first input features (I_1)
+    in_features_2 : int
+        The number of second input features (I_2)
+    out_features : int
+        The number of output features (O)
+    bias : bool, optional
+        Whether or not to include a bias (the default is True)
+    """
+    def __init__(self, in_features_1, in_features_2, out_features, bias=True):
+        super(BiLinear, self).__init__()
+        self.in_features_1 = in_features_1
+        self.in_features_2 = in_features_2
+        self.out_features = out_features
+        self.W = Parameter(np.zeros([out_features, in_features_1, in_features_2]))
+        if bias:
+            self.b = Parameter(np.zeros(out_features))
+        else:
+            self.b = None
+        self.reset_parameters()
+
+    def forward(self, x1, x2):
+        self.x1 = x1
+        self.x2 = x2
+        z = x1 @ self.W @ x2
+        if self.b is not None:
+            z += self.b
